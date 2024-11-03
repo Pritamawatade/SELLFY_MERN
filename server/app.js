cfc4618083fc0b65a9ba577fbdf96868a47fe2a1
pritamawatade <pritamawatade5@gmail.com>
2024-11-03 21:34:49 +0530

    started backend

---
+++ server/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+require('dotenv/config')
+
+app.use(cors())
+app.options('*', cors())
+
+
+app.use(bodyParser.json()) 
+ 
+//Routes 
+
+const CatagoryRoutes = require('./routes/category')
+
+//Databse connection 
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() =>{
+    console.log('Connected to MongoDB')
+})
+.catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message)
+})
+
+app.listen(process.env.PORT, () =>{
+    console.log(`app is listening on port${process.env.PORT}`);
+    
+})
+
+
+//video no 20 18 min
---
+++ server/routes/category.js
@@ -0,0 +1,22 @@
+const { Category }  = require('../models/category.js')
+
+const express = require('express')
+
+const router = express.Router()
+
+// router.get(`/`
+
+// Database
+
+
+
+
+
+
+
+
+
+
+
+
+
