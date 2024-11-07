70c6a49e19eea9d0a2107afe8b4e3dbd793a6520
pritamawatade <pritamawatade5@gmail.com>
2024-11-07 20:45:57 +0530

    succufully added profile dropdown

---
+++ admin/src/component/SearchBox/index.js
@@ -0,0 +1,14 @@
+import { IoSearchOutline } from "react-icons/io5";
+
+
+const SearchBox =  () =>{
+    return (
+        <div className="searchBox relative d-flex align-items-center">
+            <IoSearchOutline />
+            <input type="text" placeholder="Search here....." />
+
+        </div>
+    )
+}
+
+export default SearchBox
