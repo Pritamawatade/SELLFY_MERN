115ec802397fc3f7af446e8310cbdea50815a07f
pritamawatade <pritamawatade5@gmail.com>
2024-11-11 21:34:42 +0530

    side bar

--- admin/src/component/Sidebar/index.js
+++ admin/src/component/Sidebar/index.js
@@ -1,38 +1,7 @@
 const Sidebar = () =>{
     return (
         <div className="sidebar">
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
-           <h1>sidebar</h1>
+
         </div>
     )
 }
