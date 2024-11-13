9169cf00a67927c89dd0cbfac8113c2dc4ee804b
pritamawatade <pritamawatade5@gmail.com>
2024-11-13 21:29:44 +0530

    structure is done

--- admin/src/pages/Dashboard/index.js
+++ admin/src/pages/Dashboard/index.js
@@ -1,21 +1,23 @@
-const Dashboard = () =>{
-    return (
-        <div>
-            <div className="right-content w-100">
-                <div className="dashboardBoxWrapper  d-flex">
-                    <div className="dashboardBox">
-                        
-                    </div>
-                    <div className="dashboardBox">
-
-                    </div>
-                    <div className="dashboardBox">
-
-                    </div>
-                </div>
+const Dashboard = () => {
+  return (
+    <div>
+      <div className="right-content w-100">
+        <div className="row">
+          <div className="col-md-8">
+            <div className="dashboardBoxWrapper d-flex">
+              <div className="dashboardBox"></div>
+              <div className="dashboardBox"></div>
+              <div className="dashboardBox"></div>
+              <div className="dashboardBox"></div>
             </div>
+          </div>
+          <div className="col-md-4">
+            <div className="box"></div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Dashboard;
