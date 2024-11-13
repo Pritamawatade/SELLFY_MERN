713b91f7bd359d7dc7d2df16306de366e2e3c08b
pritamawatade <pritamawatade5@gmail.com>
2024-11-13 21:14:39 +0530

    added nothing

--- admin/src/pages/Dashboard/index.js
+++ admin/src/pages/Dashboard/index.js
@@ -1,7 +1,19 @@
 const Dashboard = () =>{
     return (
         <div>
-            
+            <div className="right-content w-100">
+                <div className="dashboardBoxWrapper  d-flex">
+                    <div className="dashboardBox">
+                        
+                    </div>
+                    <div className="dashboardBox">
+
+                    </div>
+                    <div className="dashboardBox">
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
