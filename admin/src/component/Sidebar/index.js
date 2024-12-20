57c3ae268657922c582c43ac908296ff31808294
pritamawatade <pritamawatade5@gmail.com>
2024-12-20 19:41:55 +0530

    add subcategory upload link to sidebar

--- admin/src/component/Sidebar/index.js
+++ admin/src/component/Sidebar/index.js
@@ -205,6 +205,13 @@ const Sidebar = () => {
               >
                 <Link to="/Category/add">Category upload</Link>
               </motion.li>
+              <motion.li 
+                variants={itemVariants}
+                whileHover={hoverAnimation}
+                whileTap={{ scale: 0.98 }}
+              >
+                <Link to="/subCategory/add">Sub Category upload</Link>
+              </motion.li>
             </motion.ul>
           </div>
         </motion.li>
